4a14bdaa2bb137cbf39af9fd097ae56da9de578e
Daniel Johnson <dan@balancerockmedia.com>
2013-08-21 17:27:18 -0400

    edits

--- ajax_demo7/shopping_list.php
+++ ajax_demo7/shopping_list.php
@@ -10,22 +10,38 @@ $items = array(
     "Seafood" => array(
         array("name" => "Salmon", "price" => 6),
         array("name" => "Scallops", "price" => 7)
+    ),
+    "Pasta" => array(
+        array("name" => "Linguine", "price" => 8)
+    ),
+    "Misc" => array(
+        array("name" => "Paper Towels", "price" => 9),
+        array("name" => "Forks", "price" => 10),
+        array("name" => "Light Bulbs", "price" => 11)
     )
 );
 
 if (isset($_REQUEST['action'])) {
     switch($_REQUEST['action']) {
-        case 'all':
-        echo json_encode($items);
-        break;
-    
-        case 'categories':
+        case 'get_categories':
         $categories = array();
         foreach($items as $key => $value) {
             $categories[] = $key;
         }
         
         echo json_encode($categories);
+        break;
+        
+        case 'get_all':
+        echo json_encode($items);
+        break;
+        
+        case 'get_by_category':
+        $category = array($_GET['category']);
+        
+        $items = array_intersect_key($items, array_flip($category));
+        
+        echo json_encode($items);
         break;
     }
 }
