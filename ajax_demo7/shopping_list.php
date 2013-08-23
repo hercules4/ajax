1d7f93678f6fed63f6a79407f1155388acf5ef57
Daniel Johnson <dan@balancerockmedia.com>
2013-08-22 22:55:53 -0400

    edits

--- ajax_demo7/shopping_list.php
+++ ajax_demo7/shopping_list.php
@@ -1,31 +1,37 @@
 <?php
 
+session_start();
+
+// unset($_SESSION['items']);
+
 header("Content-type: application/json");
 
-$items = array(
-    "Produce" => array(
-        array("name" => "Broccoli", "price" => 4),
-        array("name" => "Carrots", "price" => 5)
-    ),
-    "Seafood" => array(
-        array("name" => "Salmon", "price" => 6),
-        array("name" => "Scallops", "price" => 7)
-    ),
-    "Pasta" => array(
-        array("name" => "Linguine", "price" => 8)
-    ),
-    "Misc" => array(
-        array("name" => "Paper Towels", "price" => 9),
-        array("name" => "Forks", "price" => 10),
-        array("name" => "Light Bulbs", "price" => 11)
-    )
-);
+if (!isset($_SESSION['items'])) {
+    $_SESSION['items'] = array(
+        "Produce" => array(
+            array("name" => "Broccoli", "price" => 4),
+            array("name" => "Carrots", "price" => 5)
+        ),
+        "Seafood" => array(
+            array("name" => "Salmon", "price" => 6),
+            array("name" => "Scallops", "price" => 7)
+        ),
+        "Pasta" => array(
+            array("name" => "Linguine", "price" => 8)
+        ),
+        "Misc" => array(
+            array("name" => "Paper Towels", "price" => 9),
+            array("name" => "Forks", "price" => 10),
+            array("name" => "Light Bulbs", "price" => 11)
+        )
+    );
+}
 
 if (isset($_REQUEST['action'])) {
     switch($_REQUEST['action']) {
         case 'get_categories':
         $categories = array();
-        foreach($items as $key => $value) {
+        foreach($_SESSION['items'] as $key => $value) {
             $categories[] = $key;
         }
         
@@ -33,19 +39,19 @@ if (isset($_REQUEST['action'])) {
         break;
         
         case 'get_all':
-        echo json_encode($items);
+        echo json_encode($_SESSION['items']);
         break;
         
         case 'get_by_category':
         $category = array($_GET['category']);
         
-        $items = array_intersect_key($items, array_flip($category));
+        $items = array_intersect_key($_SESSION['items'], array_flip($category));
         
         echo json_encode($items);
         break;
         
         case 'add_item':
-        $items[$_POST['category']][] = array('name' => $_POST['item'], 'price' => intval($_POST['price']));
+        $_SESSION['items'][$_POST['category']][] = array('name' => $_POST['item'], 'price' => intval($_POST['price']));
         
         echo 1;
         break;
